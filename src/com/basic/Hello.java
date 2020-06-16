c3f6f77356583a531046b8f6459009b57f3151b5
CHANDRA SHEKHAR TIWARI <chandra@ficode.in>
2020-06-16 22:12:39 +0530

    basic2

--- src/com/basic/Hello.java
+++ src/com/basic/Hello.java
@@ -1,8 +1,14 @@
 package com.basic;
 
+import java.util.Scanner;
+
 public class Hello {
 
     public static void main(String[] args) {
 	    System.out.println("Hello Java Programming Language");
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println("My Name is :- "+s);
     }
 }
