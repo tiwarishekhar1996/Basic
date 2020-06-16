6014ff609b3912c3360cf853a53fc555060325ff
CHANDRA SHEKHAR TIWARI <chandra@ficode.in>
2020-06-16 22:06:42 +0530

    basic1

--- src/com/basic/Hello.java
+++ src/com/basic/Hello.java
@@ -3,6 +3,6 @@ package com.basic;
 public class Hello {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Hello Java Programming Language");
     }
 }
---
+++ src/com/basic/NumberCount.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class NumberCount {
+    public static void main(String[] args) {
+        int i;
+        for (i=1; i<=100;i++) {
+            System.out.println(i);
+        }
+    }
+}
